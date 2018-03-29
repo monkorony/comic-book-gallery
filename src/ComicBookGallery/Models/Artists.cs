f3986667718c18459e00e0676cae9f52ce094e1c
Sirin Srikier <ssrikier@gmail.com>
2018-03-28 20:19:45 -0700

    Updated comic book model

---
+++ src/ComicBookGallery/Models/Artists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artists
+    {
+        public string name { get; set; }
+        public string role { get; set; }
+    }
+}
