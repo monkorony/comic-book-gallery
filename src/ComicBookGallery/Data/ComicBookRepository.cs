716c3e01f70b12d3717a9e022f3f7de05c4c6f4e
Sirin Srikier <ssrikier@gmail.com>
2018-04-01 21:44:09 -0700

    Added view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -57,7 +57,10 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
         };
-
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
